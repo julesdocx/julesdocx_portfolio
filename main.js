94c10c9f1190ee60aee1fa7d352bdd334fa905fa
docxjules <jules.docx@gmail.com>
2020-09-30 11:15:23 +0200

    Title and grid

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log('hello world')
+
+let timeline = anime.timeline({
+    duration: 4000,
+    easing: 'easeInOutQuart'
+})
+
+timeline.add({
+    targets: 'svg',
+    keyframes: [
+        {scale:0.3},
+        {scale: 1}
+    ],
+    duration: 2500
+})
+
+timeline.add({
+    targets: ['feTurbulence', 'feDisplacementMap'],
+    baseFrequency: 0, 
+    numOctaves: 0,
+    scale: 1,
+}, '-=4000')
