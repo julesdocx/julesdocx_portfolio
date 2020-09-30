8398d04529eeecc8f302aba7962c7f418db89179
docxjules <jules.docx@gmail.com>
2020-09-30 18:23:15 +0200

    svg backgroung

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ let timeline = anime.timeline({
     duration: 3000,
     easing: 'easeInOutQuart'
 }).add({
+    targets:['rect'],
+    keyframes: [
+        {height:1},
+        {height:3000}
+    ],
+    background:'#5e5e5e'
+}).add({
     targets: ['#heySVG'],
     keyframes: [
         {scale:0},
