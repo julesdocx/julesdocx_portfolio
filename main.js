9925b2f06cedcf5b28a69d09801e085ab81ea67b
docxjules <jules.docx@gmail.com>
2020-09-30 13:18:57 +0200

    svg background

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 console.log('hello world')
 
 let timeline = anime.timeline({
-    duration: 4000,
+    duration: 3000,
     easing: 'easeInOutQuart'
-})
-
-timeline.add({
-    targets: 'svg',
+}).add({
+    targets: ['#heySVG'],
     keyframes: [
-        {scale:0.3},
-        {scale: 1}
+        {scale:0},
+        {scale:90}
     ],
     duration: 2500
-})
-
-timeline.add({
+}).add({
     targets: ['feTurbulence', 'feDisplacementMap'],
     baseFrequency: 0, 
     numOctaves: 0,
     scale: 1,
-}, '-=4000')
+}, '-=3000')
+
+
